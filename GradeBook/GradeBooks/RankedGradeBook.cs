1ee508c8913c52dfe9826183abaae6e46c249425
yairglassm <yairglassman@gmail.com>
2021-10-11 13:52:05 +0300

    step 2
    step 2

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,17 @@
+﻿using GradeBook.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    public class StandardGradeBook : BaseGradeBook
+    {
+        public StandardGradeBook(string name) : base(name)
+        {
+            Name = name;
+            Type = GradeBookType.Standard;
+            Students = new List<Student>();
+        }
+    }
+}
---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,17 @@
+﻿using GradeBook.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    public class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook(string name) : base(name)
+        {
+            Name = name;
+            Type = GradeBookType.Ranked;
+            Students = new List<Student>();
+        }
+    }
+}
